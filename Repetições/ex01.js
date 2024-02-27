a8c91599c64c00a060eb093f3a0434d493a5d339
Junior Amorim <junioramorim144@gmail.com>
2024-02-27 14:57:27 -0300

    estudos

---
+++ Repetições/ex01.js
@@ -0,0 +1,19 @@
+var  none = 1
+
+while (none <= 10 ) {
+    console.log(`Funciona ${none} while`)
+    none++
+}
+
+
+do { console.log (`funciona ${none} do while`)
+    none++
+
+} while (none <= 15)   
+
+
+for ( ; none <= 20; none++ ) {
+
+    console.log (`Funciona ${none} for `)
+
+}
